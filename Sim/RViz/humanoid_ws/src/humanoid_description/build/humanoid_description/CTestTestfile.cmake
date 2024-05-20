2fea4eb60f3cf71450c79f950a002d391329539e
Priyanshupareek <ppareek85@gmail.com>
2024-05-20 15:45:27 +0530

    Torso and waist Simulation in RViz
    Set up a humanoid xacro file that links (for now) torso and the waist for joint manipulation and simulation.

---
+++ Sim/RViz/humanoid_ws/src/humanoid_description/build/humanoid_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gateway/humanoid_ws/humanoid_description
+# Build directory: /home/gateway/humanoid_ws/humanoid_description/build/humanoid_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
